92640e14989cb501f19d7285a78c29dff8f94075
mayankchoubey <choubey@gmail.com>
2021-11-24 11:21:16 -0800

    Update router.ts

--- router.ts
+++ router.ts
@@ -1,14 +1,8 @@
 import { Status } from "https://deno.land/std/http/http_status.ts";
-import {HTTP_METHOD_GET} from "./constants.ts";
-import {getFile} from "./controller.ts";
-import {sendResponseCode} from "./utils.ts";
+import { handleRequest } from "./controller.ts";
 
-export async function handleRequest(req: Request, resp: any) {
-    if(req.method !== HTTP_METHOD_GET)
-        return sendResponseCode(resp, Status.MethodNotAllowed);
-    const u=new URL(req.url);
-    if(u.pathname === '/')
-        return sendResponseCode(resp, Status.NotFound);
-    await getFile(u.pathname, resp);
+export async function route(req:Request):Promise<Response> {
+    if(req.method !== 'GET')
+        return new Response(null, {status: Status.MethodNotAllowed});
+    return await handleRequest(req);
 }
-
