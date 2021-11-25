038367d3e44da09688d796e805487c9669ab55fa
mayankchoubey <choubey@gmail.com>
2021-11-24 20:40:19 -0800

    Update authService.ts

--- authService.ts
+++ authService.ts
@@ -1,19 +1,24 @@
-import { ENV_API_KEYS_PATH, API_KEYS_PATH, ENV_NO_AUTH } from "./consts.ts";
+import { API_KEYS_PATH, ENV_API_KEYS_PATH, ENV_NO_AUTH } from "./consts.ts";
 
-let apiKeys:Array<string>=[];
+let apiKeys: Array<string> = [];
 try {
-    apiKeys=JSON.parse(Deno.readTextFileSync(Deno.env.get(ENV_API_KEYS_PATH) || API_KEYS_PATH));
-} catch(e) {}
+    apiKeys = JSON.parse(Deno.readTextFileSync(Deno.env.get(ENV_API_KEYS_PATH) || API_KEYS_PATH));
+} catch (_e) {
+    console.info('No API keys found, authentication is disabled');
+}
 
-export function authorize(headers:Headers) {
-    const authorized=true, notAuthorized=false;
-    if(Deno.env.get(ENV_NO_AUTH) || !apiKeys.length)
+export function authorize(headers: Headers) {
+    const authorized = true, notAuthorized = false;
+    if (Deno.env.get(ENV_NO_AUTH) || !apiKeys.length) {
         return authorized;
-    const authHeader=headers.get('Authorization');
-    if(!authHeader)
+    }
+    const authHeader = headers.get("Authorization");
+    if (!authHeader) {
         return notAuthorized;
-    const apiKey=authHeader.split(" ")[1];
-    if(!apiKey)
+    }
+    const apiKey = authHeader.split(" ")[1];
+    if (!apiKey) {
         return notAuthorized;
+    }
     return apiKeys.includes(apiKey);
 }
