5673fc356141fead0c5b540c8e9e3d75fb29dca3
mayankchoubey <choubey@gmail.com>
2021-11-24 20:42:03 -0800

    Update router.ts

--- router.ts
+++ router.ts
@@ -1,8 +1,9 @@
 import { Status } from "https://deno.land/std/http/http_status.ts";
 import { handleRequest } from "./controller.ts";
 
-export async function route(req:Request):Promise<Response> {
-    if(req.method !== 'GET')
-        return new Response(null, {status: Status.MethodNotAllowed});
+export async function route(req: Request): Promise<Response> {
+    if (req.method !== "GET") {
+        return new Response(null, { status: Status.MethodNotAllowed });
+    }
     return await handleRequest(req);
 }
