a727aed7eda3ab551921b1b0509acf1cea850ea1
Mayank Choubey <mayank.choubey@ushur.com>
2021-12-01 11:04:46 -0800

    .

--- deploy/fileService.ts
+++ deploy/fileService.ts
@@ -1,15 +1,10 @@
-import { readableStreamFromReader as makeStream } from "https://deno.land/std/streams/mod.ts";
 import { SERVE_PATH } from "./consts.ts";
 
 export async function getContent(relativePath: string) {
     const filePath = SERVE_PATH + relativePath;
-    const fileData = await Deno.stat(filePath);
-    if (!fileData.isFile) {
-        throw new Deno.errors.BadResource("Directories cannot be served");
-    }
-    const r = await Deno.open(filePath);
+    const file = await Deno.readFile(filePath);
     return {
-        len: fileData.size,
-        content: makeStream(r),
+        len: file.length,
+        content: file,
     };
 }
